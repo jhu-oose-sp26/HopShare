7fb518c19849b21733916f47d27496df4fbccf2e
Immanuel Gebreyesus <gt9322@gmail.com>
2026-02-22 16:28:06 -0500

    add primitive CRUD operations

---
+++ backend/backend_readme.md
@@ -0,0 +1,4 @@
+1. install mongodb at this [link](https://www.mongodb.com/try/download/compass)
+2. start a server at the default localhost
+3. make an .env file based off of .env.example to contain all environment variables
+4. check out the package.json to see the pnpm commands
--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,17 @@
 const express = require('express');
-const { connectDB, getDB } = require('./db');
+const cors = require('cors');
+const { connectDB } = require('./db');
+const postsRoutes = require('./routes/posts');
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
+app.use('/posts', postsRoutes);
 
-connectDB().then(async () => {
-  console.log('Database is connected');
-
-  const db = getDB();
-  const postsCollection = db.collection('posts');
-
-  // one mock post
-  // placeholder IDs for now (creatorId and trip)
-  await postsCollection.insertOne({
-    creatorId: 1,        // placeholder until we create users
-    content: 'This is the first post in HopShare!',
-    trip: null           // placeholder until trips exist
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
   });
-
-  console.log('Inserted a mock post');
-
-  // route that gets all posts
-  app.get('/posts', async (req, res) => {
-    const posts = await postsCollection.find().toArray();
-    res.json(posts);
-  });
-
-  app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 });
