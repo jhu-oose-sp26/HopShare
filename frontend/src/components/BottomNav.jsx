39569055af3e8a31a8f6c573866dcd99efad6da1
Janna <jannaibrahim25@gmail.com>
2026-04-09 16:44:35 -0400

    space out nav bar elements

--- frontend/src/components/BottomNav.jsx
+++ frontend/src/components/BottomNav.jsx
@@ -11,7 +11,7 @@ const BottomNav = () => {
 
   return (
     <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 z-50">
-      <div className="flex justify-around items-center h-16 max-w-lg mx-auto">
+      <div className="flex justify-around items-center h-16">
         {navItems.map(({ to, icon: Icon, label }) => (
           <NavLink
             key={to}
